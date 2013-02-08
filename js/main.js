3c24dd064e5cd5094eeb3b116f4dde09c8fd5924
Timothy Keith <timothy@keithieopia.com>
2013-02-08 15:56:42 -0500

    Added jQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.9.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
