fabe9467778a7767223f47c93d650b01bb4d7faa
Timothy Keith <timothy@keithieopia.com>
2013-04-01 10:58:37 -0400

    Separated out the credits and 404 page to their own files. Added the start of a login page.

--- index.php
+++ index.php
@@ -2,28 +2,33 @@
 
 	include 'inc/bootstrap.php';
 
-
 	if (!isset($_GET['p'])) $_GET['p'] = 'index';
 
-	$file = 'text/'. $_GET['p'] .'.md';
-			
-	if(file_exists($file)){
-		$c['content'] = Markdown(file_get_contents($file));
-	}
-	else {
-		header('HTTP/1.0 404 Not Found');
-        die('<h1>File Not Found</h1>
-             <p>
-                You enter a dark and musty smelling room and fumble for a light switch. 
-                A dim bare blub hanging precariously from the ceiling turns on. You find 
-                yourself surrounded by moldy manila folders, rusty old file cabinets, and 
-                one stale grilled cheese sandwich. This must be the place... the legendary 
-                File Not Found page rarely spoken of. 
-             </p>
-            ');
+
+    switch ($_GET['p']){
+
+        case 'credits':
+            $c['content'] = file_get_contents('html/credits.html');
+            break;
         
-	}
-	
+        case 'login':
+            $c['content'] = file_get_contents('html/login.html');
+            break;
+            
+        default: 
+            
+            $file = 'text/'. $_GET['p'] .'.md';
+			
+            if(file_exists($file)){
+                $c['content'] = Markdown(file_get_contents($file));
+            }
+            else {
+                header('HTTP/1.0 404 Not Found');
+                $c['content'] = file_get_contents('html/404.html');
+            }
+    }
+    
+    
 	$c['runtime'] = round(((microtime(TRUE) - $c['runtime']) * 1000), 3);
     $c['peakram'] = humanFilesize(memory_get_peak_usage(TRUE));
 
