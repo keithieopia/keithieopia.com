8a0de667ea6f6013981a12c4b0770b45d576ee8e
Timothy Keith <timothykeith@gmail.com>
2016-04-23 15:09:34 -0400

    Old celtic theme, semi-broken

--- content/themes/celtic/js/init.js
+++ content/themes/celtic/js/init.js
@@ -1,28 +1,42 @@
+var THEME_NAME = $('#theme-name').text();
+
 // Load Link Labeler
-loadresource('content/themes/default/js/label-links.js');
+loadresource('content/themes/' + THEME_NAME + '/js/label-links.js');
 
 // Load Piwik Stats
-loadresource('content/themes/default/js/piwik.js');
+loadresource('content/themes/' + THEME_NAME + '/js/piwik.js');
 
 // -=[ LAZY LOADER ]===============================================================================================-
 console.log('INFO: Lazy loading JS & CSS based on elements in page...');
 
 // Tooltip Popups
 if ($('.tooltip-text').length != 0) {
-    loadresource('content/themes/default/css/tooltip.css');
-    loadresource('content/themes/default/js/tooltip.js');
+    loadresource('content/themes/' + THEME_NAME + '/css/tooltip.css');
+    loadresource('content/themes/' + THEME_NAME + '/js/tooltip.js');
 }
 
-// FAQ Answer Hider / Shower
+// Back to Top of Page
+if ($('#go-to-top').length != 0) {
+    loadresource('content/themes/' + THEME_NAME + '/css/go-to-top.css');
+    loadresource('content/themes/' + THEME_NAME + '/js/go-to-top.js');
+}
+
+// Random Quote Rotator
+if ($('#random-quote').length != 0) {
+    loadresource('content/themes/' + THEME_NAME + '/css/rand-quote.css');
+    loadresource('content/themes/' + THEME_NAME + '/js/rand-quote.js');
+}
+
+// FAQ Answer Hider / Shower 
 if ($('.faq').length != 0) {
-    loadresource('content/themes/default/css/faq.css');
-    loadresource('content/themes/default/js/faq.js');
+    loadresource('content/themes/' + THEME_NAME + '/css/faq.css');
+    loadresource('content/themes/' + THEME_NAME + '/js/faq.js');
 }
 
 // Source Code Highlighter
 if ($('pre code').length != 0) {
-    loadresource('content/themes/default/css/highlight/tomorrow-night.css');
-    loadresource('content/themes/default/js/highlight.js');
+    loadresource('content/themes/' + THEME_NAME + '/css/highlight/tomorrow-night.css');
+    loadresource('content/themes/' + THEME_NAME + '/js/highlight.js');
 
     $('pre code').each(function(i, e) { hljs.highlightBlock(e) });
 }
